adad5fe6de59c7ffacebdd9d014c734add1a76d7
Sapphire0912 <iris228520@gmail.com>
2024-03-13 13:37:25 +0800

    Practice

--- FullStack Lesson/Ch7 Document Object Model/element_object.js
+++ FullStack Lesson/Ch7 Document Object Model/element_object.js
@@ -1,7 +1,7 @@
 let myButton = document.querySelector("#my-btn");
-myButton.addEventListener("click", () => {
-  alert("點擊了 button");
-});
+// myButton.addEventListener("click", () => {
+//   alert("點擊了 button");
+// });
 
 let body = document.querySelector("body");
 let myH4 = document.createElement("h4");
@@ -15,11 +15,11 @@ body.appendChild(myH3);
 
 // classList 屬性: 可以取得當前 element 的 classList (列表)
 let p_tag = document.querySelector(".hello");
-console.log(p_tag.classList);
+// console.log(p_tag.classList);
 p_tag.classList.add("evening");
-console.log(p_tag.classList);
+// console.log(p_tag.classList);
 p_tag.classList.remove("morning");
-console.log(p_tag.classList);
+// console.log(p_tag.classList);
 
 // 每觸發一次事件, 就會讓 p_tag 的 class, 新增 -> 刪除 red class -> 新增 -> 刪除 ...
 // 類似開燈關燈的功能, 數位邏輯的雙穩態電路
@@ -28,4 +28,17 @@ p_tag.addEventListener("click", () => {
 });
 
 // 檢查 p_tag 是否包含 red 的 class
-console.log(p_tag.classList.contains("red"));
+// console.log(p_tag.classList.contains("red"));
+
+// let a = document.querySelector("a");
+// console.log(a.getAttribute("href"));
+
+let button = document.querySelector("button");
+button.style.backgroundColor = "green";
+button.style.color = "white";
+// button.style = "font-size: 1.05rem"; 或是把想要的東西全部寫在 style 屬性裡
+
+button.addEventListener("click", () => {
+  let a = document.querySelector("a");
+  a.remove();
+});
