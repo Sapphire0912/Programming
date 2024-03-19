844c529c9fb802dfa458f3ac1603f757dc30fbde
Sapphire0912 <iris228520@gmail.com>
2024-03-19 09:27:35 +0800

    Project

--- FullStack Lesson/Project/Project 4/app.js
+++ FullStack Lesson/Project/Project 4/app.js
@@ -33,3 +33,138 @@ time_line
 window.setTimeout(() => {
   animation.style.pointerEvents = "none";
 }, 2500);
+
+// 避免使用者按 Enter 將表單的內容全部送出
+window.addEventListener("keypress", (e) => {
+  // 按下 Enter 鍵: keypress { target: body, key: "Enter", charCode: 0, keyCode: 13 }
+  // console.log(e);
+  if (e.key == "Enter") {
+    e.preventDefault();
+  }
+});
+
+// 防止點選垃圾桶的 button 將整個表單內容送出
+let allButtons = document.querySelectorAll("button");
+allButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+// 改變 <select> 裡面的 <option> color
+let allSelects = document.querySelectorAll("select");
+allSelects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    // 讀取是哪個 option 被選取
+    // console.log(e.target.value);
+
+    // 每調整一次成績, 就會動態調整 GPA 內容. selectedIndex 讀取哪個 option 被選取
+    setGPA();
+    changeColor(e.target); // e.target 就是 select 標籤
+  });
+});
+
+// 改變 credit 之後, GPA 也要更新
+let credits = document.querySelectorAll(".class-credit");
+credits.forEach((credit) => {
+  credit.addEventListener("change", () => {
+    setGPA();
+  });
+});
+
+function changeColor(target) {
+  if (target.value == "A" || target.value == "A-") {
+    target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
+  } else if (
+    target.value == "B+" ||
+    target.value == "B" ||
+    target.value == "B-"
+  ) {
+    target.style.backgroundColor = "yellow";
+    target.style.color = "black";
+  } else if (
+    target.value == "C+" ||
+    target.value == "C" ||
+    target.value == "C-"
+  ) {
+    target.style.backgroundColor = "orange";
+    target.style.color = "black";
+  } else if (
+    target.value == "D+" ||
+    target.value == "D" ||
+    target.value == "D-"
+  ) {
+    target.style.backgroundColor = "red";
+    target.style.color = "black";
+  } else {
+    target.style.backgroundColor = "gray";
+    target.style.color = "white";
+  }
+}
+
+function convertor(grade) {
+  switch (grade) {
+    case "A":
+      return 4.0;
+    case "A-":
+      return 3.7;
+    case "B+":
+      return 3.4;
+    case "B":
+      return 3.0;
+    case "B-":
+      return 2.7;
+    case "C+":
+      return 2.4;
+    case "C":
+      return 2.0;
+    case "C-":
+      return 1.7;
+    case "D+":
+      return 1.4;
+    case "D":
+      return 1.0;
+    case "D-":
+      return 0.7;
+    case "F":
+      return 0.0;
+    default:
+      return 0;
+  }
+}
+
+function setGPA() {
+  let formLength = document.querySelectorAll("form").length;
+  let credits = document.querySelectorAll(".class-credit");
+  let selects = document.querySelectorAll(".select");
+  let sum = 0;
+  let creditSum = 0;
+
+  for (let i = 0; i < credits.length; i++) {
+    // credits[i].value 回傳的是 string
+    if (!isNaN(credits[i].valueAsNumber)) creditSum += credits[i].valueAsNumber;
+  }
+
+  for (let i = 0; i < formLength; i++) {
+    if (!isNaN(credits[i].valueAsNumber)) {
+      sum += credits[i].valueAsNumber * convertor(selects[i].value);
+    }
+  }
+
+  let GPA;
+  if (creditSum == 0) {
+    GPA = 0.0;
+  } else {
+    GPA = (sum / creditSum).toFixed(2);
+  }
+  document.getElementById("result-gpa").innerText = GPA;
+}
+
+// 每次重新整理視窗就將 form 的內容清除
+window.onload = function () {
+  let form = document.querySelectorAll("form");
+  form.forEach((f) => {
+    f.reset();
+  });
+};
