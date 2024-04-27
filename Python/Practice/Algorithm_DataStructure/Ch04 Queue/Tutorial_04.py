c3b830d0feb399af946d981fe41ac4993737ccb0
Sapphire0912 <iris228520@gmail.com>
2024-04-27 11:23:12 +0800

    ALG Practice

--- Python/Practice/Algorithm_DataStructure/Ch04 Queue/Tutorial_04.py
+++ Python/Practice/Algorithm_DataStructure/Ch04 Queue/Tutorial_04.py
@@ -20,18 +20,34 @@ class MyQueue(object):
         return len(self.queue)
 
 
-q = MyQueue()
-q.enqueue('Grape')
-q.enqueue('Mango')
-q.enqueue('Apple')
-# print("the length of queue is ", q.size())
-# for i in range(0, q.size()):
-#     print("Queue content: ", q.dequeue())
+# q = MyQueue()
+# q.enqueue('Grape')
+# q.enqueue('Mango')
+# q.enqueue('Apple')
+# # print("the length of queue is ", q.size())
+# # for i in range(0, q.size()):
+# #     print("Queue content: ", q.dequeue())
+#
+# # 上面的 code 等同於下面的效果
+# q2 = Queue()
+# q2.put("Grape")
+# q2.put("Mango")
+# q2.put("Apple")
+# while not q2.empty():
+#     print(q2.get())
 
-# 上面的 code 等同於下面的效果
-q2 = Queue()
-q2.put("Grape")
-q2.put("Mango")
-q2.put("Apple")
-while not q2.empty():
-    print(q2.get())
+
+
+from collections import deque
+
+deque_ = deque([1, 2, 3, 4, 5, 6, 7, 8])
+deque_2 = deque([])
+target = 4
+
+while deque_:
+    element = deque_.popleft()
+    if element != target:
+        deque_2.append(element)
+
+deque_2.insert(4, target)
+print(deque_2)
