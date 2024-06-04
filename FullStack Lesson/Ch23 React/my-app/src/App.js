a2a60e15eb954f7757ce75fbe25efdb1c66165d4
Sapphire0912 <iris228520@gmail.com>
2024-06-05 06:37:38 +0800

    React Practice

--- FullStack Lesson/Ch23 React/my-app/src/App.js
+++ FullStack Lesson/Ch23 React/my-app/src/App.js
@@ -1,7 +1,34 @@
+import Nav from "./Nav";
+import Info from "./info";
+
 function App() {
+  const buttonHandler = (msg) => {
+    alert(msg);
+  };
+
+  let gem = ["Sapphire", "Ruby"];
+  let gem_info = [
+    { gem_name: "Sapphire", price: "160K" },
+    { gem_name: "Ruby", price: "200K" },
+  ];
+
   return (
     <div>
       <h1>這是 App.js 的 h1 Tag.</h1>
+      <Nav />
+      {/* <Info name={gem[0]} price={"160K"} />
+      <Info name={gem[1]} price={"200K"} /> */}
+      {gem_info.map((obj) => {
+        return <Info name={obj.gem_name} price={obj.price} />;
+      })}
+
+      <button
+        onClick={() => {
+          buttonHandler("今天天氣不錯");
+        }}
+      >
+        Click me!
+      </button>
     </div>
   );
 }
