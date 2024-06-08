714c22f1488568da99ba572278e1c9624aa86286
Sapphire0912 <iris228520@gmail.com>
2024-06-08 21:37:58 +0800

    Project 8

---
+++ FullStack Lesson/Project/Project 8/img-website/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import HomePage from "./HomePage";
+import About from "./About";
+import Page404 from "./Page404";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="*" element={<Page404 />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
